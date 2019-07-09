57ecc4ae0d20377dcf69719ad2a25f61158ec672
Boccuzzi <boccuzzig@wit.edu>
2019-07-09 14:53:15 -0400

    working bottom navigation bar and tabs with each are functional

---
+++ MD4/app/src/main/java/edu/wit/mobilepp/md3/CollectionFragment.java
@@ -0,0 +1,38 @@
+package edu.wit.mobilepp.md3;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTabHost;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CollectionFragment extends Fragment {
+    private FragmentTabHost mTabHost;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mTabHost = new FragmentTabHost(getActivity());
+        mTabHost.setup(getActivity(), getChildFragmentManager(), R.layout.fragment_collection);
+
+        Bundle arg1 = new Bundle();
+        arg1.putInt("Arg for Frag1", 1);
+        mTabHost.addTab(mTabHost.newTabSpec("Tab1").setIndicator("Makeup"),
+                CollectionFragment_MakeupTab.class, arg1);
+
+        Bundle arg2 = new Bundle();
+        arg2.putInt("Arg for Frag2", 2);
+        mTabHost.addTab(mTabHost.newTabSpec("Tab2").setIndicator("Skincare"),
+                CollectionFragment_SkincareTab.class, arg2);
+
+        return mTabHost;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mTabHost = null;
+    }
+}
---
+++ MD4/app/src/main/java/edu/wit/mobilepp/md3/CollectionFragment_SkincareTab.java
@@ -0,0 +1,19 @@
+package edu.wit.mobilepp.md3;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CollectionFragment_SkincareTab extends Fragment  {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View V = inflater.inflate(R.layout.fragment_collection_tab2_skincare, container, false);
+
+        return V;
+    }
+}
