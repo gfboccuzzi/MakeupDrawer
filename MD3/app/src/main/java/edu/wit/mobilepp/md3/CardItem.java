77549535e5628acb08f37119f9a342f0a4b77b0c
boccuzzig <boccuzzig@wit.edu>
2019-07-15 11:50:17 -0400

    added List functionality to Makeup Collection tab, being entered manually now, want to move to button functionality

---
+++ MD3/app/src/main/java/edu/wit/mobilepp/md3/CardItem.java
@@ -0,0 +1,11 @@
+package edu.wit.mobilepp.md3;
+
+import android.graphics.Bitmap;
+
+public class CardItem {
+    public Bitmap image;
+    public String brand;
+    public String product;
+    public String category;
+    public String shade;
+}
---
+++ MD3/app/src/main/java/edu/wit/mobilepp/md3/CardItemAdapter.java
@@ -0,0 +1,47 @@
+package edu.wit.mobilepp.md3;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class CardItemAdapter extends ArrayAdapter<CardItem> {
+    private LayoutInflater mInflater;
+    public CardItemAdapter(Context context, int rid, List<CardItem> list){
+        super(context, rid, list);
+        mInflater =
+                (LayoutInflater)context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+    }
+    public View getView(int position, View convertView, ViewGroup parent){
+        // Retrieve data
+        CardItem item = (CardItem)getItem(position);
+        // Use layout file to generate View
+        View view = mInflater.inflate(R.layout.card_item, null);
+        // Set image
+        ImageView image;
+        image = (ImageView)view.findViewById(R.id.image);
+        image.setImageBitmap(item.image);
+        // Set title
+        TextView brand;
+        brand = (TextView)view.findViewById(R.id.brand);
+        brand.setText(item.brand);
+        // Set description
+        TextView product;
+        product = (TextView) view.findViewById(R.id.product);
+        product.setText(item.product);
+
+        TextView category;
+        category = (TextView) view.findViewById(R.id.category);
+        category.setText(item.category);
+
+        TextView shade;
+        shade = (TextView) view.findViewById(R.id.shade);
+        shade.setText(item.shade);
+        return view;
+    }
+}
